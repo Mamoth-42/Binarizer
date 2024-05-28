eca77ffc418c8fed079234f4f2585ba7a0607511
Mamoth-42 <mamoth662@gmail.com>
2024-05-28 15:38:32 +0200

    Add project files.

---
+++ Binarizer/Program.cs
@@ -0,0 +1,26 @@
+﻿
+int DecimalToBinary(int num, bool verbose)
+{
+    var binar = new Stack<int>();
+    int outValue = 0;
+    int rem = 0;
+    while (num > 0)
+    {
+        rem = num % 2;
+        num = num / 2;
+        binar.Push(rem);
+    }
+    binar.Reverse();
+    while (binar.Count() > 0)
+    {
+        if (verbose)
+            Console.Write(binar.Peek());
+        if (binar.Pop() == 1)
+            outValue++;
+    }
+    if (verbose)
+        Console.WriteLine("");
+    return outValue;
+}
+
+Console.WriteLine(DecimalToBinary(42,true));
